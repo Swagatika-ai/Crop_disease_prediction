6daaabcdf8d71c1cf24ea3705d46efdad922300f
Swagatika-ai <56960265+Swagatika-ai@users.noreply.github.com>
2021-03-05 14:51:23 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,17 +8,18 @@ import seaborn as sns
 from io import BytesIO,StringIO
 import requests
 from PIL import Image,ImageOps
+import pickle as pkl
 
 
 
 
 
 def main():
-    st.title("Crop Disease Prediction")
+    #st.title("AI Demo")
     #st.subheader("High Level Analysis")
     html_temp = """ 
     <div style="background.color:green;padding:15px;">
-    <h2>Disease classify</h2>
+    <h2>Bean Image Classifier</h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
@@ -31,18 +32,21 @@ def main():
 main()
     
 
-MODEL_PATH="model_inception.h5"
+MODEL_PATH="model_mob.h5"
 from keras.models import load_model
-model = load_model(MODEL_PATH)
+import tensorflow_hub as hub
+model = load_model(MODEL_PATH,custom_objects={'KerasLayer':hub.KerasLayer})
 @st.cache(allow_output_mutation=True)
 
 def load_model():
-    model = tf.keras.models.load_model(MODEL_PATH)
+    model = tf.keras.models.load_model(MODEL_PATH,custom_objects={'KerasLayer':hub.KerasLayer})
     return model
 
 
 
-classes = ["Diseased cotton leaf","Diseased cotton plant","Fresh cotton leaf","Fresh cotton plant"]
+
+
+classes = ["angular_leaf_spot","bean_rust","Healthy"]
 
 def scale(image):
     image = tf.cast(image,tf.float32)
@@ -55,8 +59,7 @@ def decode_img(image):
     img = scale(img)
     return np.expand_dims(img,axis=0)
 
-#with st.spinner("Loading Model into Memory......"):
-    #model = load_model()  
+#path = st.text_input("Enter Image URL to classify..")
 
 def file_selector(folder_path='.'):
     filenames = os.listdir(folder_path)
@@ -87,6 +90,11 @@ st.write('You selected `%s`' % filename)
 image = Image.open(filename)
 st.image(image,use_column_width=True)
 
+#label = np.argmax(model.predict(decode_img(image)),axis=1)
+#st.write(classes[label[0]])
+#st.write("")
+#img = Image.open(BytesIO(image))
+#st.image(img,caption="Classifying Bean Image",use_column_width=True)
 prediction =import_and_predict(image,model)
 string ="This image most likely is: " +classes[np.argmax(prediction)]
 st.success(string)
